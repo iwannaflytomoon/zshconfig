33d004781caa761eda04d55a6d40eea5a25f6c21
iwannaflytomoon <iwannaflytomoon@gmail.com>
2016-01-07 18:24:55 +0800

    增加了tmux配置文件

--- install.sh
+++ install.sh
@@ -4,6 +4,8 @@ mv ~/.zshrc ~/.zshrc.orig
 ln -s $PWD/zshrc ~/.zshrc
 ln -s $PWD/half-life_kenan.zsh-theme ~/.oh-my-zsh/themes/half-life_kenan.zsh-theme
 ln -s $PWD/gitconfig ~/.gitconfig
+ln -s $PWD/tmux.conf ~/.tmux.conf
 git clone git://github.com/jimmijj/zsh-syntax-highlighting ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 git clone git://github.com/rimraf/k ~/.oh-my-zsh/custom/plugins/k
 git clone git://github.com/rupa/z ~/.oh-my-zsh/custom/plugins/z
+
